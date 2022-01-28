bedf1468fa6852d1b1d44800cdf2d659496c726f
tylerBender6 <tbender6@husker.unl.edu>
2022-01-28 16:04:43 +0000

    intial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Tyler Bender
+ *         tbender6@huskers.unl.edu
+ * Date: 2022/01/28
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Tyler Bender\n");
+
+  return 0;
+}
